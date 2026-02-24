1ed764d4dee96fd80a85da03e33e0525ba5920ba
AKumar188 <1rn21is188.anishk@gmail.com>
2026-02-24 14:48:15 +0530

    Fixed payment migration dependency

--- payments/migrations/0001_initial.py
+++ payments/migrations/0001_initial.py
@@ -12,9 +12,9 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('subscriptions', '0001_initial'),
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-    ]
+    migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ('subscriptions', '0001_initial'),
+]
 
     operations = [
         migrations.CreateModel(
